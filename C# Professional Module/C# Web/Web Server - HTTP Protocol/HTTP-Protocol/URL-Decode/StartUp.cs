91c5d2f30b4b95bab4e027322749a5d5f101045c
adskidolen <alex_speedy@mail.bg>
2018-09-21 01:30:13 +0300

    SoftUni

---
+++ C# Professional Module/C# Web/Web Server - HTTP Protocol/HTTP-Protocol/URL-Decode/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace URL_Decode
+{
+    using System;
+    using System.Net;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string inputURL = Console.ReadLine();
+
+            string decodedURL = WebUtility.UrlDecode(inputURL);
+
+            Console.WriteLine(decodedURL);
+        }
+    }
+}
---
+++ C# Professional Module/C# Web/Web Server - HTTP Protocol/HTTP-Protocol/ValidateURL/StartUp.cs
@@ -0,0 +1,80 @@
+﻿namespace ValidateURL
+{
+    using System;
+    using System.Net;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string inputURL = Console.ReadLine();
+
+            string invalidURL = "Invalid URL";
+
+            var decodedURL = WebUtility.UrlDecode(inputURL);
+
+            try
+            {
+                var parsedURL = new Uri(decodedURL);
+
+                var sb = new StringBuilder();
+
+                var protocol = parsedURL.Scheme;
+                var host = parsedURL.Host;
+                var port = parsedURL.Port;
+                var path = parsedURL.AbsolutePath;
+
+                var query = parsedURL.Query;
+                query = query.Replace("?", "");
+                query = query.Replace("&amp;", "&");
+
+                var fragment = parsedURL.Fragment;
+                fragment = fragment.Replace("#", "");
+
+                if (string.IsNullOrWhiteSpace(protocol))
+                {
+                    Console.WriteLine(invalidURL);
+                    Environment.Exit(0);
+                }
+
+                var validUrlForHttp = protocol == "http" && port == 80;
+                var validUrlForHttps = protocol == "https" && port == 443;
+
+                if (!validUrlForHttp && !validUrlForHttps)
+                {
+                    Console.WriteLine(invalidURL);
+                    Environment.Exit(0);
+                }
+
+                if (string.IsNullOrWhiteSpace(host))
+                {
+                    Console.WriteLine(invalidURL);
+                    Environment.Exit(0);
+                }
+
+                sb.AppendLine($"Protocol: {protocol}");
+                sb.AppendLine($"Host: {host}");
+                sb.AppendLine($"Port: {port}");
+                sb.AppendLine($"Path: {path}");
+
+                if (!string.IsNullOrWhiteSpace(query))
+                {
+                    sb.AppendLine($"Query: {query}");
+                }
+
+                if (!string.IsNullOrWhiteSpace(fragment))
+                {
+                    sb.AppendLine($"Fragment: {fragment}");
+                }
+
+                string result = sb.ToString().TrimEnd();
+                Console.WriteLine(result);
+            }
+            catch
+            {
+                Console.WriteLine(invalidURL);
+            }
+        }
+    }
+}
